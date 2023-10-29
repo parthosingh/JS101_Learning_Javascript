513c9a4aff87d90d4bf3061005aebb929152e92a
parthosingh <parthosingh792@gmail.com>
2023-10-29 06:01:21 +0530

    day one task completed

---
+++ Day1/intro.js
@@ -0,0 +1,13 @@
+//keywords to decalre a variable that are let, var and const
+let a;  //declare a variable
+a = 5;  // assigning a value to the variable.
+let x = 10;  //declare a varaible and assign the value in the same line.
+//console.log(a); // console means to see the output
+//console.log(x);
+
+var sum = 20;
+console.log(sum);
+
+var name = "Tanya"; // Anything between double (" ") quotes is  a string "1" is also a string because it is in double quotes.
+const variable = "hello";
+console.log(variable)
---
+++ Day1/variable1.js
@@ -0,0 +1,14 @@
+//let num = 15;
+
+//console.log(typeof(num));
+//let num = 15;
+//num = "Hello";
+//console.log(typeof(num))
+
+//let num = 14;
+//num = "20";
+//console.log(typeof(num))
+let num = 14;
+ num = "20";
+ let ans = +(20);
+console.log(ans)
